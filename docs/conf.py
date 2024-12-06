3e7342476396c4e76601286546e16b478de11a3e
Marko Baric <marko.baric@pasteur.fr>
2024-12-06 10:59:23 +0100

    fix(docs):conf

--- docs/conf.py
+++ docs/conf.py
@@ -11,25 +11,22 @@ version = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'myst_parser',  # For markdown support with Sphinx 3.0+
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',  # Include documentation from docstrings
+    'sphinx.ext.napoleon',  # Support for Google-style docstrings
+    'sphinx.ext.viewcode',  # Add links to highlighted source code
+    'sphinx.ext.todo',      # Support for todo items
 ]
 
 templates_path = ['_templates']
-source_suffix = ['.rst', '.md']  # Include Markdown files
+source_suffix = '.rst'  # Only use RestructuredText
 master_doc = 'index'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'  # Use Read the Docs theme
+html_theme = 'sphinx_rtd_theme'  # Use the Read the Docs theme
 html_static_path = ['_static']
 
 # -- Extension configuration -------------------------------------------------
-# If you have your own conf.py tweaks, they can go here
 
 # -- Options for LaTeX output ------------------------------------------------
 latex_elements = {
@@ -38,15 +35,6 @@ latex_elements = {
     'preamble': '',
     'figure_align': 'htbp'
 }
-
-# -- Options for intersphinx -------------------------------------------------
-intersphinx_mapping = {'python': ('https://docs.python.org/3/', None)}
-
-# -- Options for todo extension ----------------------------------------------
-todo_include_todos = True
-
-# -- Options for Read the Docs PDF generation --------------------------------
-latex_engine = 'xelatex'  # Use XeLaTeX for better Unicode support
 
 # -- Read the Docs configuration ---------------------------------------------
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
